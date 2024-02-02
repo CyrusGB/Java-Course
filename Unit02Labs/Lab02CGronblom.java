a36df94429e3542cef103608a0e795a0f61bf219
Cyrus <cjgronblom@gmail.com>
2024-02-02 11:16:42 -0500

    Update Lab02CGronblom.java

--- Unit02Labs/Lab02CGronblom.java
+++ Unit02Labs/Lab02CGronblom.java
@@ -27,56 +27,85 @@ public class Lab02CGronblom extends Canvas {
 		int width = 980;
 		int height = 630;
 		int x1 = 10;
-		int y1 = 640;
-		int x2 = 990;
-		int y2 = 640;
+		int y1 = (10);
+      g.setColor(Color.red);
+      g.drawOval(x1 -5, y1-5, 10, 10); //DEbug check values
+		int x2 = (width + 10);
+		int y2 = (height + 10);
+      g.setColor(Color.black);
+      g.drawOval(x2 -5, y2-5, 10, 10); //DEbug check values
+
 		g.drawRect(10,10,width,height);
       
       int loopAmt = 50; // Amount of times the loop iterates through drawing lines
 
       //code for lower right corner
-      int xInd = 10;
-      int yInd = 0;
+
+
+      int xInd = x1;
+      int yInd = y2;
+      g.setColor(Color.red);
       for (int i = 0; i < loopAmt; i++) {
-         g.drawLine(xInd, y1, x2, yInd + y2);
-         xInd += 990/(loopAmt - 1);
-         yInd -= 640/(loopAmt - 1);
+         g.drawLine(xInd, y2, x2, yInd);
+         xInd += (width + 10) / (loopAmt - 1);
+         yInd -= (height + 10) / (loopAmt - 1);
       }
       
       //code for lower left corner
-      xInd = 990;
-      yInd = 0;
-      for (int i = 0; i < loopAmt; i++) {
-         g.drawLine(xInd, y1, x1, yInd + y1);
-         xInd -= 990/(loopAmt - 1);
-         yInd -= 640/(loopAmt - 1);
-      }
-      
+
       //code for upper right corner
-      xInd = 10;
-      yInd = 0;
-      for (int i = 0; i < loopAmt; i++) {
-         g.drawLine(xInd, 10, x2, yInd + 10);
-         xInd += 990/(loopAmt - 1);
-         yInd += 640/(loopAmt - 1);
-      }
-      
+
       //code for upper left corner
-      
       
       //******* Code for innerbox ***********
-      
+      width = 490;
+		height = 315;
+		x1 = 490/2 + 10;
+		y1 = 315/2 + 325;
+      // g.drawOval(x1 -5, y1-5, 10, 10); Debug to check x1/y1
+		x2 = x1 + 490;
+		y2 = y1;
+		g.drawRect(490 / 2 + 10, 315 / 2 + 10, width, height);
       
       //code for lower right corner
-      
+      xInd = x1;
+      yInd = 0;
+      g.setColor(Color.red);
+      for (int i = 0; i < loopAmt; i++) {
+         g.drawLine(xInd, y1, x2, yInd + y2);
+         xInd += (width + 10) / (loopAmt - 1);
+         yInd -= (height + 10) / (loopAmt - 1);
+      }
       
       //code for lower left corner
-      
+      xInd = x2;
+      yInd = 0;
+      g.setColor(Color.magenta);
+      for (int i = 0; i < loopAmt; i++) {
+         g.drawLine(xInd, y1, x1, yInd + y1);
+         xInd -= (width + 10) / (loopAmt - 1);
+         yInd -= (height + 10) / (loopAmt - 1);
+      }
       
       //code for upper right corner
-      
+      xInd = x1;
+      yInd = 0;
+      g.setColor(Color.blue);
+      for (int i = 0; i < loopAmt; i++) {
+         g.drawLine(xInd, 10, x2, yInd + 10);
+         xInd += (width + 10) / (loopAmt - 1);
+         yInd += (height + 10) / (loopAmt - 1);
+      }
       
       //code for upper left corner
+      xInd = (width + 10);
+      yInd = 0;
+      g.setColor(Color.orange);
+      for (int i = 0; i < loopAmt; i++) {
+         g.drawLine(xInd, 10, x1, yInd + 10);
+         xInd -= (width + 10) / (loopAmt - 1);
+         yInd += (height + 10) / (loopAmt - 1);
+      }
       
 	}
    //this is a delay method that will allow you to watch your program do its drawing
@@ -104,6 +133,25 @@ public class Lab02CGronblom extends Canvas {
       while (endDelay - startDelay < delaytime)
          endDelay = System.currentTimeMillis();
 
+   }
+
+   public static void drawCorner(Graphics g, Boolean flipX, Boolean flipY, int x1, int y1, int width, int height, int loopAmt){
+      int x2 = x1 + width;
+      int y2 = y1 + height;
+      int xInd = x1;
+      int yInd = y2;
+      g.setColor(Color.red);
+      if(flipX){
+            
+      }
+      if(flipY){
+
+      }
+      for (int i = 0; i < loopAmt; i++) {
+         g.drawLine(xInd, y2, x2, yInd);
+         xInd += (width + 10) / (loopAmt - 1);
+         yInd -= (height + 10) / (loopAmt - 1);
+      }
    }
 
         
