37de31b0ac3aedb783e88888ae2705043719728e
Cyrus <cjgronblom@gmail.com>
2024-02-12 11:03:12 -0500

    Update Lab3CGronblom.java

--- Unit03Labs/Lab3CGronblom.java
+++ Unit03Labs/Lab3CGronblom.java
@@ -1,6 +1,6 @@
 // Lab03CStudentLastName.java
 // Student starting version of the Lab03C assignment.
-// Student Name:             Date:
+// Student Name: Cyrus Gronblom Date: 2/12/24
 // Purpose of Program: 
 // Using the Math.random() method to randomly draw lines, circles, 
 // and squares within a set grid area  
@@ -23,7 +23,7 @@ public class Lab3CGronblom extends Canvas {
         
    }
 
-   public int getRandom(int min, int max){
+   public int getRandom(int min, int max){ // Randomly generates a number, needs the min and max boundaries
       int range = max - min + 1;
       int random = (int)(Math.random() * range + min);
       return random;
@@ -38,35 +38,62 @@ public class Lab3CGronblom extends Canvas {
 		
 		
 		// Draw Random Lines
-      for (int i = 0; i < 100; i++) {
+      for (int i = 0; i < 101; i++) { // Draw 100 random lines with random colors
          Color color = new Color(getRandom(1, 255), getRandom(1, 255), getRandom(1, 255));
          g.setColor(color);
          g.drawLine(getRandom(10, 400), getRandom(10, 300), getRandom(10, 400), getRandom(10, 300));
       }
+      delay();
 
 		// Draw Random Squares
-      for (int i = 0; i < 100; i++) {
+      for (int i = 0; i < 101; i++) { // Draw 100 randomly colored rectangles
          Color color = new Color(getRandom(1, 255), getRandom(1, 255), getRandom(1, 255));
          g.setColor(color);
-         g.drawRect(getRandom(400, 740), getRandom(10, 250), 50, 50);
+         g.fillRect(getRandom(400, 740), getRandom(10, 250), 50, 50);
       }
-
-
-
+      delay();
 		
 		// Draw Random Circles
-
-
-
-
+      for (int i = 0; i < 101; i++) { // Draw 100 randomly sized and colored circles
+         Color color = new Color(getRandom(1, 255), getRandom(1, 255), getRandom(1, 255));
+         g.setColor(color);
+         int diameter = getRandom(0, 200);
+         g.drawOval(getRandom(10, (400 - diameter)), getRandom(300, (590 - diameter)), diameter, diameter);
+      }
+      delay();
 		
 		// Draw 3-D Box
-	
-	
-	
-	
-	
+      int x = 575, y = 425; // Position of the box
+      int height = 100, width = 100, depth = 100; // Dimensions of the box
+      
+      Polygon right = new Polygon(); // Right side
+      right.addPoint(x + width, y);
+      right.addPoint(x + width - depth / 2, y);
+      right.addPoint(x + width - depth / 2, y - depth / 2);
 
+      g.setColor(Color.blue); // Draw right side
+      g.fillPolygon(right);
+
+      Polygon back = new Polygon(); // Back 
+      back.addPoint(x - depth / 2 , y - depth / 2);
+      back.addPoint(x + width - depth / 2, y - depth / 2);
+      back.addPoint(x + width - depth / 2, y);
+      back.addPoint(x, y);
+
+      g.setColor(Color.yellow); // Draw back
+      g.fillPolygon(back);
+
+      Polygon left = new Polygon(); // Left side
+      left.addPoint(x,y);
+      left.addPoint(x, y + height);
+      left.addPoint(x - depth / 2, y + depth / 2);
+      left.addPoint(x - depth / 2, y - depth / 2);
+      
+      g.setColor(Color.green); // Draw left
+      g.fillPolygon(left);
+
+      g.setColor(Color.red); // Draw front
+      g.fillRect(x, y, width, height);
    }
 
    //this is a delay method that will allow you to watch your program do its drawing
